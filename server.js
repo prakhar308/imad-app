c47a73c2dadce82bda3eefd3e631b05255b77a87
Prakhar Khandelwal <36433443+prakhar308@users.noreply.github.com>
2018-02-21 19:41:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get('/article-one', function(req, res){
     res.send(' Article one requested and will be served')
 });
 
-app.get('/article-one', function(req, res){
-    res.send(' Article one requested and will be served')
+app.get('/article-two', function(req, res){
+    res.send(' Article two requested and will be served')
 });
 
-app.get('/article-one', function(req, res){
-    res.send(' Article one requested and will be served')
+app.get('/article-three', function(req, res){
+    res.send(' Article three requested and will be served')
 });
 
 
