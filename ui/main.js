67784e282dd97449456f44158ad4bf85493c0d27
Prakhar Khandelwal <36433443+prakhar308@users.noreply.github.com>
2018-03-02 19:14:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML='New Text'
 
 var img = document.getElementById('madi');
 
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft+'px';
+}
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,50);
 }
