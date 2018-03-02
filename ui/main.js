49a0e21f0b705017e6b4e218ca27b0a8ffe23186
Prakhar Khandelwal <36433443+prakhar308@users.noreply.github.com>
2018-03-02 19:09:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element =document.getElementById('main-text');
 element.innerHTML='New Text'
+
+var img = document.getElementById('madi');
+
+img.onclick = function(){
+    img.style.marginLeft='100px';
+}
